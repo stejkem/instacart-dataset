7d18f5ac544ab22716caba2e559ab3d872c3f4b0
Stejkem <stejkem@gmail.com>
2017-11-27 20:26:39 +0200

    Simplify startup tool

--- tools/startup.py
+++ tools/startup.py
@@ -1,28 +1,18 @@
-import urllib.request
 import tarfile
 import os
 
 from pathlib import Path
 
-URL_FOR_DATA_SET = open("dataset_url.txt", "r").read()
 DATA_SET_ARCHIVE_FILE_NAME = "instacart_online_grocery_shopping_2017_05_01.tar.gz"
-
-data_set_directory = Path("instacart_2017_05_01")
-data_set_archive = Path(DATA_SET_ARCHIVE_FILE_NAME)
+DATA_SET_DIRECTORY_NAME = "instacart_2017_05_01"
 
 os.chdir("..")
-if data_set_directory.is_dir():
+if Path(DATA_SET_DIRECTORY_NAME).is_dir():
     print("The data set has been already extracted.")
+elif Path(DATA_SET_ARCHIVE_FILE_NAME).is_file():
+    print("The data set archive has not been extracted yet.")
+    print("Extracting the archive...")
+    tarfile = tarfile.open(DATA_SET_ARCHIVE_FILE_NAME, "r:gz")
+    tarfile.extractall(".")
 else:
-    if data_set_archive.is_file():
-        print("The data set archive has been already downloaded.")
-        print("Extracting the archive...")
-        tarfile = tarfile.open(DATA_SET_ARCHIVE_FILE_NAME, "r:gz")
-        tarfile.extractall(".")
-        print("Extraction is completed!")
-    else:
-        print("The data set archive has not been downloaded yet.")
-        print("Downloading the archive...")
-        print("The approximate size is 200MB...")
-        urllib.request.urlretrieve(URL_FOR_DATA_SET, filename=DATA_SET_ARCHIVE_FILE_NAME)
-        print("Download is completed!")
+    print("The date set archive is not downloaded! Please, do it manually.")

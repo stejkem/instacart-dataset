f880d753870c5bd23ab2a5685281515a644ffd9c
Stejkem <stejkem@gmail.com>
2017-11-27 20:32:29 +0200

    Refactor startup tool

---
+++ resources/constants.py
@@ -0,0 +1,2 @@
+DATA_SET_ARCHIVE_FILE_NAME = "instacart_online_grocery_shopping_2017_05_01.tar.gz"
+DATA_SET_DIRECTORY_NAME = "instacart_2017_05_01"
--- tools/startup.py
+++ tools/startup.py
@@ -1,18 +1,17 @@
 import tarfile
 import os
+import resources.constants as constants
 
 from pathlib import Path
 
-DATA_SET_ARCHIVE_FILE_NAME = "instacart_online_grocery_shopping_2017_05_01.tar.gz"
-DATA_SET_DIRECTORY_NAME = "instacart_2017_05_01"
-
 os.chdir("..")
-if Path(DATA_SET_DIRECTORY_NAME).is_dir():
+if Path(constants.DATA_SET_DIRECTORY_NAME).is_dir():
     print("The data set has been already extracted.")
-elif Path(DATA_SET_ARCHIVE_FILE_NAME).is_file():
+elif Path(constants.DATA_SET_ARCHIVE_FILE_NAME).is_file():
     print("The data set archive has not been extracted yet.")
     print("Extracting the archive...")
-    tarfile = tarfile.open(DATA_SET_ARCHIVE_FILE_NAME, "r:gz")
+    tarfile = tarfile.open(constants.DATA_SET_ARCHIVE_FILE_NAME, "r:gz")
     tarfile.extractall(".")
 else:
-    print("The date set archive is not downloaded! Please, do it manually.")
+    print("The data set archive is not downloaded!")
+    print("Please, get it manually from https://www.instacart.com/datasets/grocery-shopping-2017.")

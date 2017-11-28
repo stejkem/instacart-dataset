90e298b49d0e54deca7127f4c2d75b3e9aa1f72c
Stejkem <stejkem@gmail.com>
2017-11-28 20:51:03 +0200

    Add simple model

--- model/data_model.py
+++ model/data_model.py
@@ -1,9 +1,13 @@
 import os
+from sklearn.naive_bayes import GaussianNB
 import model.dataset_tools as tl
 
 os.chdir("..")
 
-test_data = tl.get_test_data()
-train_data = tl.get_train_data()
+clf = GaussianNB()
+train_order_hour_of_day, train_product_id = tl.get_train_data(10000)
+clf.fit(train_order_hour_of_day, train_product_id.ravel())
+print("The model is ready.")
 
-print("The data set has been processed.")
+test_order_hour_of_day, test_product_id = tl.get_test_data(1000)
+print("The current model's score is", clf.score(test_order_hour_of_day, test_product_id))
--- model/dataset_tools.py
+++ model/dataset_tools.py
@@ -1,17 +1,21 @@
 import pandas as pd
 import resources.constants as constants
 
+product_columns = ["order_id", "product_id"]
 
-def get_train_data():
+
+def get_train_data(nrows=None):
     order_products_train_file_path = constants.DATA_SET_DIRECTORY_NAME + "/" + 'order_products__train.csv'
-    order_products_train_df = pd.read_csv(order_products_train_file_path, sep=',', usecols=["order_id", "product_id"])
-    return pd.merge(order_products_train_df, get_orders_df())
+    order_products_train_df = pd.read_csv(order_products_train_file_path, sep=',', usecols=product_columns, nrows=nrows)
+    train_data = pd.merge(order_products_train_df, get_orders_df())
+    return train_data[['order_hour_of_day']].values, train_data[['product_id']].values
 
 
-def get_test_data():
+def get_test_data(nrows=None):
     order_products_test_file_path = constants.DATA_SET_DIRECTORY_NAME + "/" + 'order_products__prior.csv'
-    order_products_test_df = pd.read_csv(order_products_test_file_path, sep=',', usecols=["order_id", "product_id"])
-    return pd.merge(order_products_test_df, get_orders_df())
+    order_products_test_df = pd.read_csv(order_products_test_file_path, sep=',', usecols=product_columns, nrows=nrows)
+    test_data = pd.merge(order_products_test_df, get_orders_df())
+    return test_data[['order_hour_of_day']].values, test_data[['product_id']].values
 
 
 def get_orders_df():

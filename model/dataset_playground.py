b496176a3fd115b17c06ba94f07e8d52cf47caae
Stejkem <stejkem@gmail.com>
2017-12-04 21:04:28 +0200

    Add heat map visualization

--- model/dataset_playground.py
+++ model/dataset_playground.py
@@ -1,12 +1,9 @@
-from sklearn.naive_bayes import GaussianNB
+import matplotlib.pyplot as plt
+import seaborn as sns
+
 import model.dataset_tools as tls
 
-train_order_hour_of_day, train_product_id = tls.get_train_data(10000)
-tls.plot_data(train_product_id, train_order_hour_of_day)
+train_data_cross_tab_df = tls.get_train_data_cross_tab_df('aisle_id', 'order_hour_of_day')
 
-# clf = GaussianNB()
-# # clf.fit(train_order_hour_of_day, train_product_id.ravel())
-# print("The model is ready.")
-#
-# test_order_hour_of_day, test_product_id = tls.get_test_data(1000)
-# print("The current model's score is", clf.score(test_order_hour_of_day, test_product_id))
+ax = sns.heatmap(train_data_cross_tab_df)
+plt.show()
--- model/dataset_tools.py
+++ model/dataset_tools.py
@@ -1,25 +1,36 @@
 import os
+
 import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
+
 import resources.constants as constants
 
-product_columns = ["order_id", "product_id"]
+PRODUCT_COLUMNS = ["order_id", "product_id"]
+
 os.chdir("..")
 
 
-def get_train_data(nrows=None):
+def get_train_data(selected_feature, selected_label, nrows=None):
     order_products_train_file_path = constants.DATA_SET_DIRECTORY_NAME + "/" + 'order_products__train.csv'
-    order_products_train_df = pd.read_csv(order_products_train_file_path, sep=',', usecols=product_columns, nrows=nrows)
-    train_data = pd.merge(order_products_train_df, get_orders_df())
-    return train_data[['order_hour_of_day']].values, train_data[['product_id']].values
+    order_products_train_df = pd.read_csv(order_products_train_file_path, sep=',', usecols=PRODUCT_COLUMNS, nrows=nrows)
+    train_data_df = pd.merge(order_products_train_df, get_products_df())
+    train_data_df = pd.merge(train_data_df, get_orders_df())
+    return train_data_df[[selected_feature]].values, train_data_df[[selected_label]].values
 
 
-def get_test_data(nrows=None):
+def get_train_data_cross_tab_df(selected_feature, selected_label, nrows=None):
+    order_products_train_file_path = constants.DATA_SET_DIRECTORY_NAME + "/" + 'order_products__train.csv'
+    order_products_train_df = pd.read_csv(order_products_train_file_path, sep=',', usecols=PRODUCT_COLUMNS, nrows=nrows)
+    train_data_df = pd.merge(order_products_train_df, get_products_df())
+    train_data_df = pd.merge(train_data_df, get_orders_df())
+    return pd.crosstab(index=train_data_df[selected_label], columns=train_data_df[selected_feature])
+
+
+def get_test_data(selected_feature, selected_label, nrows=None):
     order_products_test_file_path = constants.DATA_SET_DIRECTORY_NAME + "/" + 'order_products__prior.csv'
-    order_products_test_df = pd.read_csv(order_products_test_file_path, sep=',', usecols=product_columns, nrows=nrows)
-    test_data = pd.merge(order_products_test_df, get_orders_df())
-    return test_data[['order_hour_of_day']].values, test_data[['product_id']].values
+    order_products_test_df = pd.read_csv(order_products_test_file_path, sep=',', usecols=PRODUCT_COLUMNS, nrows=nrows)
+    test_data_df = pd.merge(order_products_test_df, get_products_df())
+    test_data_df = pd.merge(test_data_df, get_orders_df())
+    return test_data_df[[selected_feature]].values, test_data_df[[selected_label]].values
 
 
 def get_orders_df():
@@ -27,7 +38,6 @@ def get_orders_df():
     return pd.read_csv(orders_file_path, sep=',', usecols=["order_id", "order_hour_of_day"])
 
 
-def plot_data(x, y):
-    plt.plot(x, y, 'ro')
-    plt.yticks([*range(24)])
-    plt.show()
+def get_products_df():
+    products_file_path = constants.DATA_SET_DIRECTORY_NAME + "/" + 'products.csv'
+    return pd.read_csv(products_file_path, sep=',', usecols=["product_id", "product_name", "aisle_id", "department_id"])
